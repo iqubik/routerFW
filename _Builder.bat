2f6e04e49a538556560f89bf1e855418d825085a
iqubik <iqubik@gmail.com>
2026-01-05 21:42:36 +0300

    v3.5

--- _Builder.bat
+++ _Builder.bat
@@ -68,7 +68,7 @@ if "%BUILD_MODE%"=="IMAGE" (
 )
 
 echo =================================================================
-echo  OpenWrt FW Builder v3.4 https://github.com/iqubik/routerFW
+echo  OpenWrt FW Builder v3.5 https://github.com/iqubik/routerFW
 echo  Текущий режим: [%MODE_TITLE%]
 echo =================================================================
 echo.
